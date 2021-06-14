dd657cf190c804d82862397ad6372e49877bede2
tang <467938742@qq.com>
2021-06-14 23:27:41 +0800

    自定义MyTypeFilter，需要继承TypeFilter，metadataReader.getClassMetadata()用来获取当前正在扫描的类的类信息

--- src/main/java/com/atguigu/config/MainConfig.java
+++ src/main/java/com/atguigu/config/MainConfig.java
@@ -1,6 +1,7 @@
 package com.atguigu.config;
 
 import com.atguigu.bean.Person;
+import com.atguigu.service.BookService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +11,14 @@ import org.springframework.stereotype.Service;
 
 //配置类==配置文件
 @Configuration // 告诉Spring这是一个配置类
-@ComponentScan(value = "com.atguigu", excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})
-})
+//@ComponentScan(value = "com.atguigu", excludeFilters = {
+//        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})
+//})
 @ComponentScan(value = "com.atguigu", includeFilters = {
-        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})
+        //这两种FilterType最常用
+//        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class}),
+//        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {BookService.class}),
+        @ComponentScan.Filter(type = FilterType.CUSTOM, classes = {MyTypeFilter.class})
 }, useDefaultFilters = false)
 //excludeFilters 指定扫描的时候按照什么规则排除哪些组件
 //includeFilters 只包含某些组件
