b237511c0c055af8640471a1cf55375aacc28836
tang <467938742@qq.com>
2021-06-14 17:25:35 +0800

    componentScan 以及 excludeFilters 还有includeFilters的使用

--- src/main/java/com/atguigu/config/MainConfig.java
+++ src/main/java/com/atguigu/config/MainConfig.java
@@ -2,10 +2,23 @@ package com.atguigu.config;
 
 import com.atguigu.bean.Person;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 //配置类==配置文件
 @Configuration // 告诉Spring这是一个配置类
+@ComponentScan(value = "com.atguigu", excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})
+})
+@ComponentScan(value = "com.atguigu", includeFilters = {
+        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})
+}, useDefaultFilters = false)
+//excludeFilters 指定扫描的时候按照什么规则排除哪些组件
+//includeFilters 只包含某些组件
+//ComponentScan是可重复注解,ComponentScans则是复数形式
 public class MainConfig {
 
     //给容器中注册一个Bean 类型为返回值的类型, id默认是用方法名作为id
---
+++ src/test/java/com.atguigu/test/IOCTest.java
@@ -0,0 +1,20 @@
+package com.atguigu.test;
+
+
+import com.atguigu.config.MainConfig;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class IOCTest {
+
+    @Test
+    @SuppressWarnings("resources")
+    public void test01() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
+        String[] definitionNames = applicationContext.getBeanDefinitionNames();
+        for(String definitionName: definitionNames) {
+            System.out.println(definitionName);
+        }
+
+    }
+}
