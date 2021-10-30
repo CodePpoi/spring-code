e898b011e1b8ed8b8f407ad571dc36db3473850e
tang <467938742@qq.com>
2021-10-30 10:44:21 +0800

    clean code

--- src/main/java/com/IOC/TBeanFactory.java
+++ src/main/java/com/IOC/TBeanFactory.java
@@ -2,8 +2,6 @@ package com.IOC;
 
 import com.IOC.beans.*;
 
-import java.io.File;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.*;
 
@@ -11,6 +9,8 @@ public class TBeanFactory {
     List<BeanInfo> beans = new ArrayList<BeanInfo>();
 
     private Map<Class, Object> typeBeanMap = new HashMap<>();
+
+    private String basePackage = "com.IOC.beans";
 
     public <T> T getBean(Class clazz) {
         return (T) typeBeanMap.get(clazz);
@@ -27,7 +27,6 @@ public class TBeanFactory {
         System.out.println(user);
     }
 
-    private String basePackage = "com.IOC.beans";
 
 
     // inject 用来给标注了AutoInject 设置值
@@ -75,46 +74,6 @@ public class TBeanFactory {
         return beans;
     }
 
-    private TClassLoader loader = new TClassLoader();
 
-    protected List<Object> findClassWithBeanAnnotation(String pathName) {
 
-        List<Object> objects = new ArrayList<>();
-        File[] files = new File(pathName).listFiles();
-        for (int i = 0; i < files.length; i++) {
-            File file = files[i];
-//            System.out.println(file.getName());
-            Object object = null;
-            try {
-                Class clazz = loader.load(file.getName());
-                //Class clazz0 = AppClassLoader.load(file.getName());
-//                clazz.
-
-//                System.out.println(clazz.getName() + clazz.isAnnotationPresent(TBean.class));
-//                Annotation[] annotations = clazz.getAnnotations();
-
-                //clazz.getFields()
-                //if (annotations.length == 0) {
-                //   objects.add(clazz.newInstance());
-                //}
-                //for (int j = 0; j < annotations.length; j++) {
-                //    String annoName = annotations[j].annotationType().getName();
-                //    System.out.println(annoName);
-                    //if(annoName.equals("beans.TBean.class")) {
-                    //    object = clazz.newInstance();
-                        //objects.add(object);
-                    //}
-                //}
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            //catch (InstantiationException e) {
-            //    e.printStackTrace();
-            //} catch (IllegalAccessException e) {
-            //    e.printStackTrace();
-            //}
-        }
-
-        return objects;
-    }
 }
